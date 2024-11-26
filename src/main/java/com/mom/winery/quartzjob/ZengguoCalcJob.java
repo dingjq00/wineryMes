e57eae04011a8e66f44b1328a60525c7a41799b0
dingjq <tianhe00@outlook.com>
2024-11-26 20:40:09 +0800

    计算装甑效率时，有可能没有酒尾的情况。

--- src/main/java/com/mom/winery/quartzjob/ZengguoCalcJob.java
+++ src/main/java/com/mom/winery/quartzjob/ZengguoCalcJob.java
@@ -40,8 +40,8 @@ public class ZengguoCalcJob implements Job {
             .query("select e from MesZengguoRecord e " +
                     "where (e.shangzengXiaolv is null or e.shangzengXiaolv <= :minXiaolv) " +
                     "and ( e.endTimeTall > :minDate ) " +
-                    "and (e.jiejiuDurationFirstClass > 0.2 or e.jiejiuDurationSecondClass > 0.2 " +
-                    "or e.jiejiuDurationThirdClass > 0.2 or e.jiejiuDurationFeishui > 0.2) " )
+                    "and (e.jiejiuDurationFirstClass > 1.5 or e.jiejiuDurationSecondClass > 1.5 " +
+                    "or e.jiejiuDurationThirdClass > 1.5 or e.jiejiuDurationFeishui > 1.5) " )
             .parameter("minXiaolv", 0.0)
             .parameter("minDate", minDate).maxResults(500)
             .list();
